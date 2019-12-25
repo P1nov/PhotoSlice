22a607c504ad213824241478b8b26333b78140a1
P1no <1473781785@qq.com>
2019-12-25 11:39:14 +0800

    Initial Commit

---
+++ PhotoSlice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoSlice
+//
+//  Created by 雷永麟 on 2019/12/25.
+//  Copyright © 2019 leiyonglin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
